436987e8d05e0cf671c006c6bf7c995cedf1c805
volodymyrd <volodymyrdotsenko@gmail.com>
2019-03-01 08:43:50 +0100

    add basic routing.

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,14 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
 
+function Index() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello App!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about/">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users/">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Route path="/" exact component={Index}/>
+                    <Route path="/about/" component={About}/>
+                    <Route path="/users/" component={Users}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
